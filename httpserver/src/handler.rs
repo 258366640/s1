6cb86b4cb521f5a697d0eedc98f3f9c04202c101
devin <dy_0912@outlook.com>
2022-06-15 22:07:20 +0800

    一些变更

---
+++ httpserver/src/handler.rs
@@ -0,0 +1,35 @@
+use http::{httprequest::HttpRequest, httpresponse::HttpResponse};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
+pub trait Handler {
+    fn handle(req: &HttpRequest) -> HttpResponse;
+
+    fn load_file(file_name: &str) -> Option<String> {
+        let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+        let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+        let full_path = format!("{}/{}", public_path, file_name);
+
+        let contents = fs::read_to_string(full_path);
+        contents.ok();
+    }
+}
+
+pub struct StaticPageHandler;
+pub struct PageNotFoundHandler;
+pub struct WebServiceHandler;
+
+#[derive(Serialize, Deserialize)]
+pub struct OrderStatus {
+    order_id: i32,
+    order_date: String,
+    order_status: String,
+}
+
+impl Hanlder for PageNotFoundHandler {
+    fn handle(_req: &HttpRequest) -> HttpResponse {
+        HttpResponse::new("404", None, Self::load_file("404.html"))
+    }
+}
